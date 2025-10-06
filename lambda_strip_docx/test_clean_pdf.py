697badd8c3a988b81e9a5e84a6094e20de415468
David McKee <david.mckee@dxw.com>
2025-10-06 18:06:39 +0100

    Use pdfcpu as our primary redaction strategy for pdfs

--- lambda_strip_docx/test_clean_pdf.py
+++ lambda_strip_docx/test_clean_pdf.py
@@ -1,19 +1,31 @@
 import clean_pdf
 
 def test_clean_pdf_works(input_pdf):
-    # alice in hex-encoded UTF-16, as it appears in qdf format
+    # alice in hex-encoded UTF-16, as it may appear in qdf format
     alice= b"0041006c006900630065"
 
-    # input pdf must be modified with `qpdf --qdf` so we can see all the data neatly
-    assert b"%QDF-1.0" in input_pdf
-    assert b"Author" in input_pdf
-    assert b"Creator" in input_pdf
-    assert b"Producer" in input_pdf
-    assert alice in input_pdf
+    # the input PDF contains Author data (via QPDF)
+    input_qdf = clean_pdf.qdf(input_pdf)
+    assert b"%QDF-1.0" in input_qdf
+    assert b"Author" in input_qdf
+    assert alice in input_qdf or b"Alice" in input_qdf
 
-    output_pdf = clean_pdf.clean(input_pdf, compressed=False)
-    assert b"%QDF-1.0" in input_pdf
-    assert b"Author" not in output_pdf
-    assert b"Creator" not in output_pdf
-    assert b"Producer" not in output_pdf
-    assert alice not in output_pdf
+    # actually process the file
+    output_pdf = clean_pdf.clean(input_pdf)
+
+    # once cleaned, QPDF outputs no metadata in QDF of processed file
+    output_qdf = clean_pdf.qdf(output_pdf)
+    assert b"%QDF-1.0" in input_qdf
+    assert b"Author" not in output_qdf
+    assert b"Alice" not in output_qdf
+    assert alice not in output_qdf
+
+    # exiftool reports no recoverable metadata
+    clean_pdf.verify_removal(output_pdf)
+
+    ## pdfcpu outputs no metadata
+    pdf_info = clean_pdf.info(output_pdf)
+    assert b"Author: \n" in pdf_info
+    assert b"Subject: \n" in pdf_info
+    assert b"PDF Producer: pdfcpu" in pdf_info
+    assert b"Title: \n" in pdf_info
