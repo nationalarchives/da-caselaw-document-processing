0688d3f4ff6e0f2481710bc1fbbd68ec78f0925a
David McKee <david.mckee@dxw.com>
2025-10-28 15:52:53 +0000

    shadowed bytes

--- lambda_strip_docx/render_pdf.py
+++ lambda_strip_docx/render_pdf.py
@@ -6,25 +6,25 @@ import pymupdf
 DEBUG_IMG_DIR = "debug-images"
 
 
-def ppm_list(bytes):
+def ppm_list(image_bytes):
     """The PPM format is very simple, just a header and pixel data.
     Two identical images should have the same PPM output, unlike other more complicated formats"""
-    doc = pymupdf.Document(stream=bytes)
+    doc = pymupdf.Document(stream=image_bytes)
     return [page.get_pixmap().tobytes("ppm") for page in doc]
 
 
-def save_pngs(bytes) -> None:
+def save_pngs(image_bytes) -> None:
     """Save PNGs for debugging purposes"""
-    doc = pymupdf.Document(stream=bytes)
+    doc = pymupdf.Document(stream=image_bytes)
     for i, page in enumerate(doc):
         Path(DEBUG_IMG_DIR).mkdir()
         with Path(f"{DEBUG_IMG_DIR}/{i}.png").open("wb") as f:
             f.write(page.get_pixmap().tobytes("png"))
 
 
-def hash_pdf_image(bytes) -> str:
+def hash_pdf_image(image_bytes) -> str:
     """Hash the PPM image of each page together, to provide a hash which should tell us if the image has changed."""
-    pages = ppm_list(bytes)
+    pages = ppm_list(image_bytes)
     m = hashlib.sha256()
     for page in pages:
         m.update(page)
